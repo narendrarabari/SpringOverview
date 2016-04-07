8b8b5ef34ef5f23a30b36a7bbad341751fdef160
narendra <narendra@192.168.0.102>
2016-04-07 17:26:08 +0530

    Constructor based dependency injection.

--- src/com/nr/entities/User.java
+++ src/com/nr/entities/User.java
@@ -6,21 +6,42 @@ package com.nr.entities;
  *
  */
 public class User {
-
+	
+	
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 	String firstName;
 	String lastName;
+	/**
+	 * 
+	 * @return FistName of the User
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	/**
+	 * 
+	 * @return LastName of the User
+	 */
 	public String getLastName() {
 		return lastName;
 	}
+	/**
+	 * 
+	 * @param lastName
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	/**
+	 * It used to print static text on the screen 
+	 * 
+	 */
 	public void demoDisplay(){
 		System.out.println("Hello World From User !!");
 	}
