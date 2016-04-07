4146bdf35a24df22a226998a39993ebe4698fb32
narendra <narendra@192.168.0.102>
2016-04-07 16:30:28 +0530

    Basic Project Setup

---
+++ src/com/nr/main/App.java
@@ -0,0 +1,13 @@
+package com.nr.main;
+/**
+ * 
+ * @author narendra rabari
+ *
+ */
+public class App {
+	
+	public static void main(String args[]){
+		System.out.println("Hello World !!");
+	}
+
+}
