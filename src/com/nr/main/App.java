f226aaceeb1700590262ecaa724a8fe39bd6b2ba
narendra <narendra@192.168.0.102>
2016-04-07 17:00:58 +0530

    Implementation of Application Context

--- src/com/nr/main/App.java
+++ src/com/nr/main/App.java
@@ -2,6 +2,9 @@ package com.nr.main;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import com.nr.entities.User;
@@ -15,8 +18,9 @@ import com.nr.entities.User;
 public class App {
 	
 	public static void main(String args[]){
-		BeanFactory beanFactory=new XmlBeanFactory(new FileSystemResource("Bean.xml"));
-		User user = (User) beanFactory.getBean("user");
+		//BeanFactory beanFactory=new XmlBeanFactory(new FileSystemResource("Bean.xml"));
+		ApplicationContext context= new ClassPathXmlApplicationContext("Bean.xml");
+		User user = (User) context.getBean("user");
 		user.demoDisplay();
 	}
 
